172d5d1565b1c166b16b683ce57e697627fb648a
Autumn_Luna <691786631@qq.com>
2026-09-23 22:38:28 +0800

    实现网关的通用类

---
+++ gatewayServer/common/callbacks.h
@@ -0,0 +1,24 @@
+#ifndef CLEARMOON_GATEWAY_COMMON_CALLBACKS_H
+#define CLEARMOON_GATEWAY_COMMON_CALLBACKS_H
+
+#include "common/buffer.h"
+#include "common/timestamp.h"
+
+#include <functional>
+#include <memory>
+
+class TcpConnection;
+
+using ReadCallback = std::function<void()>;
+using WriteCallback = std::function<void()>;
+using ErrorCallback = std::function<void()>;
+
+using TcpConnectionPtr = std::shared_ptr<TcpConnection>;
+
+using ConnectionCallback = std::function<void(const TcpConnectionPtr&)>;
+using MessageCallback = std::function<void(const TcpConnectionPtr&, Buffer*, Timestamp)>;
+using WriteCompleteCallback = std::function<void(const TcpConnectionPtr&)>;
+
+using CloseCallback = std::function<void(const TcpConnectionPtr&)>;
+
+#endif
---
+++ gatewayServer/common/currentThread.cc
@@ -0,0 +1,21 @@
+#include "common/currentThread.h"
+
+#include <cstdio>
+#include <sched.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+
+
+thread_local int t_cacheTid = 0;
+thread_local char t_tidString[32];
+thread_local int t_tidStringLength = 6;
+thread_local const char* t_threadName = nullptr;
+
+void cacheTid()
+{
+    if(t_cacheTid == 0)
+    {
+        t_cacheTid = static_cast<pid_t>(::syscall(SYS_gettid));
+        t_tidStringLength = snprintf(t_tidString, sizeof t_tidString, "%5d ", t_cacheTid);
+    }
+}
